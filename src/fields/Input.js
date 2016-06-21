4226b0182df7c7fc3f5765b41d042f0a881c4e29
Ashley Blurton <ashley.blurton@thinmartian.com>
2016-06-21 17:24:45 +0100

    Initial fields

---
+++ src/fields/Input.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from "react";
+import classnames from "classnames";
+
+import Field from "../Field";
+
+class Input extends Field {
+  static propTypes = {
+    ...Field.propTypes,
+    className: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    ...Field.defaultProps,
+    className: "Input",
+    type: "text",
+  };
+
+  render() {
+    const { className: cls, type } = this.props;
+    const value = this.getValue();
+    const error = this.error();
+    const errorKey = this.errorKey();
+    const valid = this.isValid() === false;
+
+    return (
+      <div
+        className={classnames(cls, {
+          [`${cls}--error`]: valid,
+          [`${cls}--working`]: this.isProcessing(),
+          [errorKey ? `${cls}--error--${errorKey}` : false]: valid,
+        })}
+      >
+        {error ? <span className={`${cls}__error`}>{error}</span> : null}
+        <input
+          className={`${cls}__control`}
+          onChange={this.handleChange}
+          type={type}
+          value={value}
+        />
+      </div>
+    );
+  }
+}
+
+export default Input;
---
+++ test/fields/InputSpec.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import Input from "../../src/fields/Input";
+
+describe("<Input /> component", () => {
+  it("should render a container with an input element and no error", () => {
+    const wrapper = shallow(<Input name="username" />);
+    expect(wrapper.is("div.Input")).to.be.true;
+    expect(wrapper.find("input.Input__control")).to.have.length(1);
+    expect(wrapper.find(".Input__error")).to.have.length(0);
+  });
+
+  it("should update it's value on change of the <input>", () => {
+    const value = "my test value";
+    const wrapper = shallow(<Input name="username" />);
+    const control = wrapper.find("input");
+    control.simulate("change", {
+      currentTarget: {
+        value,
+      },
+    });
+    expect(wrapper.instance().getValue()).to.equal(value);
+  });
+
+  it("should update the input value when setting internal value", () => {
+    const value = "my test value";
+    const wrapper = mount(<Input name="a" />);
+    const control = wrapper.find("input");
+    wrapper.instance().setValue(value);
+    expect(control.prop("value")).to.be.equal(value);
+  });
+
+  it("should set an error class when invalid", () => {
+    const wrapper = shallow(<Input name="a" />);
+    wrapper.instance().setValid(false);
+    wrapper.update();
+    expect(wrapper.is(".Input--error")).to.be.true;
+  });
+
+  it("should display an error message when invalid", () => {
+    const wrapper = shallow(<Input name="a" />);
+    wrapper.instance().setValid(false, "example error msg");
+    wrapper.update();
+    expect(wrapper.find(".Input__error")).to.have.length(1);
+    expect(wrapper.find(".Input__error").text()).to.equal("example error msg");
+  });
+
+  it("should set an error class when pending async changes", () => {
+    const Async = () => new Promise(resolve => setTimeout(resolve, 10));
+    const wrapper = shallow(
+      <Input name="a">
+        <Async />
+      </Input>
+    );
+    wrapper.instance().validate();
+    wrapper.update();
+    expect(wrapper.is(".Input--working")).to.be.true;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        wrapper.update();
+        expect(wrapper.is(".Input--working")).to.be.false;
+        resolve();
+      }, 11);
+    });
+  });
+
+  it("should set a specific error class when a key is provided", () => {
+    const wrapper = shallow(<Input name="a" />);
+    wrapper.instance().setValid(false, "Field is required", "req");
+    wrapper.update();
+    expect(wrapper.is(".Input--error--req")).to.be.true;
+  });
+});
