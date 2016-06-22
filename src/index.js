d843b8947a40569c499ade093a3e7273ad6b3ed9
Ashley Blurton <ashley.blurton@thinmartian.com>
2016-06-22 11:41:18 +0100

    Fix NODE_ENV polyfill to work with define plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 global.process = process || {};
 process.env = process.env || {};
-process.env.NODE_ENV = process.env.NODE_ENV || "production";
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = "production";
+}
 
 export Form from "./Form";
 export Field from "./Field";
-export * from "./rules";
+export Input from "./fields/Input";
